4483f5d0077f5f42362c3e279b98347a62706b63
Andrea Cannon <cannonac1049@gmail.com>
2024-02-13 08:08:09 -0600

    while loops and for of practice

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -50,9 +50,23 @@ console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
+  let index = 0;
+  while (index < stars.length) {
+  console.log(stars[index]);
+  index++;
+  }
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
+  let i=0;
+  while (i<6) {
+  i++;
+  }
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+  let count = 10; 
+  while (count >= 5) {
+  console.log(count);
+  count--;
+  }
